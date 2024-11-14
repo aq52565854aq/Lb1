ab979d3b9a684ceeea4cb3edfa59d5f8797de7e2
aq52565854aq <166018292+aq52565854aq@users.noreply.github.com>
2024-11-14 20:08:31 +0200

    lb1

--- Hello world2.py
+++ Hello world2.py
@@ -1,15 +1,21 @@
 import requests
 import matplotlib.pyplot as plt
-
 resp = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20241020&end=20241024&valcode=USD&sort=exchangedate&order=desc&json")
-response_dict = resp.json()[0]
-
+print(resp)
+print(resp.content)
+print(resp.json())
+response_dict= resp.json()[0]
+print(response_dict)
+for iten in response_dict:
+    print(iten)
+print("----")
+for iten in resp.json():
+     print(iten['exchangedate'], iten['rate'])
 dates = []
 rates = []
 for item in resp.json():
     dates.append(item['exchangedate'])
     rates.append(item['rate'])
-
 plt.plot(dates, rates)
 plt.xlabel('Дата')
 plt.ylabel('Курс (UAH/USD)')
